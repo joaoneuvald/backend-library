29ff1f8291f13fe7fea09417d5ae67d6c8c13e7c
Neuvald <joaoneuvald@outlook.com>
2025-04-30 22:50:54 -0300

    seeder control

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,11 +7,17 @@ while ! nc -z db 5432; do
   sleep 2
 done
 
-echo "DB's ready. Running migrations and seeders..."
+echo "DB's ready. Running migrations..."
 
 sleep 2
 
 php artisan migrate --force
-php artisan db:seed --force
+
+if ! php artisan app:check-seeder; then
+  echo "Running seeders..."
+  php artisan db:seed --force
+else
+  echo "All seeders ran."
+fi
 
 php artisan serve --host=0.0.0.0 --port=8000
